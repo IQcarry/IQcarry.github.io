200b1ac5595219f402cb9cc91ce87a3e6ae04104
赵梓淇 <76978322+IQcarry@users.noreply.github.com>
2022-08-31 12:27:14 +0800

    Add files via upload

---
+++ js/head.js
@@ -0,0 +1,12 @@
+function toindex(){
+    location.href="index.html"
+}
+function toresume(){
+    location.href="resume.html"
+}
+function toblog(){
+    location.href="https://www.cnblogs.com/iqcarry/"
+}
+function togithub(){
+    location.href="https://github.com/IQcarry"
+}
